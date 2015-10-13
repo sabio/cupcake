493cb22b06a4121ab3cd92f8f901bebb260e08f7
armando <armando@localhost.localdomain>
2015-10-12 19:58:40 -0500

    Arreglando cosas

--- cupcake/settings.py
+++ cupcake/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'gettingstarted.urls'
+ROOT_URLCONF = 'cupcake.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gettingstarted.wsgi.application'
+WSGI_APPLICATION = 'cupcake.wsgi.application'
 
 
 # Database
