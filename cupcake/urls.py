4e32a632fcf33f1633a2ab392a1fa24c1df66bb0
armando <armando@localhost.localdomain>
2016-05-08 18:52:22 -0500

    tratando de resolver estaticos

--- cupcake/urls.py
+++ cupcake/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import settings 
 
 
 import hello.views
 import sitio.views
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', sitio.views.index, name='index'),
@@ -27,5 +30,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', sitio.views.login, name='login'),
     url(r'^prueba$', hello.views.index, name='index2'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
 
 ]
+
