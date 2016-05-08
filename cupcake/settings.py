6e72b9c98567e76fafe5efaa61bb49951968556a
armando <armando@localhost.localdomain>
2016-05-08 18:18:14 -0500

    tratando de resolver estaticos

--- cupcake/settings.py
+++ cupcake/settings.py
@@ -183,7 +183,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfilesx')
 
 STATICFILES_DIRS = (
     BASE_DIR, 'staticfiles'
