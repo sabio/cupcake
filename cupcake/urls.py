e5842728c7685c702bfbfbccc1667cbac85cdfe4
armando <armando@localhost.localdomain>
2016-05-08 19:27:53 -0500

    probando algo en urls

--- cupcake/urls.py
+++ cupcake/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', sitio.views.login, name='login'),
     url(r'^prueba$', hello.views.index, name='index2'),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
+    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
 
 ]
 
