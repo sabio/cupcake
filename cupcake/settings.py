9b3a5944b2bd0749076d44d5828b7e2037aa2f9f
armando <armando@localhost.localdomain>
2016-05-12 16:46:02 -0500

    Se quita la carpeta de upload

--- cupcake/settings.py
+++ cupcake/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%1ephpn((mopqs4v*1*(m%6imik1(td0(602b5+@6&sm)1r_e6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = []
@@ -77,30 +77,26 @@ WSGI_APPLICATION = 'cupcake.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-"""
-
-
-
-
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd7h3k6h1jg61g3',
-        'USER': 'avcqayrpxqjeqv',
-        'PASSWORD': 'p6TX2Vd4Bqm0nvzuHWS29g0-4p',
-        'HOST': 'ec2-54-227-253-238.compute-1.amazonaws.com',
-        'PORT': '5432',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd7h3k6h1jg61g3',
+            'USER': 'avcqayrpxqjeqv',
+            'PASSWORD': 'p6TX2Vd4Bqm0nvzuHWS29g0-4p',
+            'HOST': 'ec2-54-227-253-238.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
     }
-}
 
 
 
