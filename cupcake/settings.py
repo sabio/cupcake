6ff8937e9253b4b129038df664d7f75ad8689944
armando <armando@localhost.localdomain>
2016-05-31 17:46:46 -0500

    Se modifica el menu de la pagina principal

--- cupcake/settings.py
+++ cupcake/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'cupcake.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
